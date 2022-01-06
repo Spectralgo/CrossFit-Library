5f16f02a71ecbbf37f92aca257056d7fc6d35ae4
Spectralgo <florian.renard@spectralgo.com>
2022-01-06 19:07:08 +0400

    added user profile to the client using vuex

--- CrossFitLibrary.Client/layouts/default.vue
+++ CrossFitLibrary.Client/layouts/default.vue
@@ -15,16 +15,25 @@
         </v-skeleton-loader>
 
         <v-skeleton-loader class="mx-1" :loading="loading"  type="button">
-          <v-btn depressed outlined v-if="authenticated">
-            <v-icon left>mdi-account-circle</v-icon>
-            Profile
-          </v-btn>
-
-
+          <div class="text-center">
+            <v-menu offset-y v-if="authenticated">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon v-bind="attrs" v-on="on">
+                  <v-icon>mdi-account-circle</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="$router.push('/profile')">
+                  <v-list-item-title>Profile</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="$auth.signoutRedirect()">
+                  <v-list-item-title>Logout</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
           <v-btn depressed outlined v-else @click="$auth.signinRedirect()">
-            <v-icon left>mdi-account-circle-outline</v-icon>sign in</v-btn>
-
-          <v-btn v-if="authenticated" depressed  @click="$auth.signoutRedirect()">Logout</v-btn>
+            <v-icon left>mdi-account-circle-outline</v-icon>Log in</v-btn>
+          </div>
         </v-skeleton-loader>
 
       </v-app-bar>
---
+++ CrossFitLibrary.Client/pages/profile/index.vue
@@ -0,0 +1,45 @@
+﻿<template>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+      <v-card-title>
+        <v-avatar>
+          <v-icon>mdi-account</v-icon>
+        </v-avatar> Test user
+      </v-card-title>
+
+    <v-card-text class="text--primary">
+      <div v-if="submissions">
+          <v-card v-for="s in submissions" :key="`${s.id}`" class="my-2" >
+            <VideoPlayer :key="`v-${s.id}`" :video="s.video"/>
+            <v-card-text>{{ s.description }}</v-card-text>
+          </v-card>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import VideoPlayer from "@/components/video-player";
+
+export default {
+  components: {
+    VideoPlayer
+  },
+  data: () => ({
+    submissions: []
+  }),
+  mounted(){
+    return this.$store.dispatch("auth/_watchUserLoaded", async () => {
+      const profile = this.$store.state.auth.profile
+      console.log('mounted profile', profile)
+      this.submissions = await this.$axios.$get(`/api/users/${profile.id}/submissions`)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
